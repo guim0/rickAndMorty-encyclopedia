98de5bf7046100f93d5cc32dbae7817e775428f7
guim0 <guimodev@gmail.com>
2023-10-19 20:08:25 -0300

    Fix: Fixing TailwindCSS on Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{dist,js,ts,tsx}"],
+  content: ["./src/**/*.{html,dist,js,ts,tsx}"],
   theme: {
     extend: {},
   },
