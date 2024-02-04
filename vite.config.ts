4884160279e4ac4452048bca14e4a14b195f2eb4
guim0 <guimodev@gmail.com>
2024-02-04 12:33:49 -0300

    :sparkles: feat - add vitest to testing

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //@ts-ignore
   test: {
     globals: true,
     environment: "jsdom",
